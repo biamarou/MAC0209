0f3983c506ee3411b4a2b9dce6dac6ec3a30de5d
leolanavo <leo0198@gmail.com>
2017-04-10 22:14:13 -0300

    Plots the sample data

--- src/arrumador_de_dados.py
+++ src/arrumador_de_dados.py
@@ -34,9 +34,17 @@ def fixer (file) :
 def main ():
 
     data = []
-    data =  fixer ("Bia andando 1.csv") # Descomente para testar
+    data =  fixer ("Bia correndo 1.csv") # Descomente para testar
+    k = 7
     for i in range (len(data) - 1):
-        plt.plot([data[i][0], data[i + 1][0]], [data[i][3],data[i + 1][3]], 'r-')
+        plt.plot([data[i][0], data[i + 1][0]], [data[i][2],data[i + 1][2]], 'r-')
+        
+    t = [0.0, 4.84, 6.35, 8.05]
+    v = [0.0, 2.54, 3.35, 3.93]
+    plt.plot([t[0] + k, t[1] + k],[v[0], v[1]/t[1]],'b-')
+    plt.plot([t[1] + k, t[2] + k],[v[1]/t[1], ((v[2] - v[1])/(t[2] - t[1]))],'b-')
+    plt.plot([t[2] + k, t[3] + k],[((v[2] - v[1])/(t[2] - t[1])), ((v[3] - v[2])/(t[3] - t[2]))],'b-')
+    
 
     plt.show()
 main()
